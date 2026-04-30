b4d6afebfa62b08a133dd4235c86e8349e12b4c4
aadishsamir123 <71179256+aadishsamir123@users.noreply.github.com>
2026-04-30 18:47:47 +0800

    fix the api endpoint url (this was the issue all along 😭)

--- api/sg/four-day-forecast.js
+++ api/sg/four-day-forecast.js
@@ -1,5 +1,5 @@
 const { proxyRealTimeApi } = require("./_proxy");
 
 module.exports = function handler(req, res) {
-  return proxyRealTimeApi(req, res, "four-day-weather-forecast");
+  return proxyRealTimeApi(req, res, "four-day-outlook");
 };
